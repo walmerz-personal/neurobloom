b4c7131e14b869f48c2ce768db7f8cb177b7a51c
Zack Walmer <walmerz@gmail.com>
2025-12-15 21:46:27 -0500

    Bump build number to 21 for TestFlight

--- app.config.js
+++ app.config.js
@@ -30,7 +30,7 @@ export default {
         ios: {
             supportsTablet: true,
             bundleIdentifier: "com.neurobloom.app",
-            buildNumber: "20",
+            buildNumber: "21",
             infoPlist: {
                 ITSAppUsesNonExemptEncryption: false
             }
