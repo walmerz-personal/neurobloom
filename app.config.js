9b41fe436473703cd0b96c651c3119afc8c4b177
Zack Walmer <walmerz@gmail.com>
2026-01-17 14:50:06 -0500

    Increment build number to 24 for HealthKit release

--- app.config.js
+++ app.config.js
@@ -32,7 +32,7 @@ export default {
         ios: {
             supportsTablet: true,
             bundleIdentifier: "com.neurobloom.app",
-            buildNumber: "23",
+            buildNumber: "24",
             infoPlist: {
                 ITSAppUsesNonExemptEncryption: false,
                 NSHealthShareUsageDescription: "NeuroBloom tracks your walking speed, steadiness, and mobility to help monitor your recovery progress.",
