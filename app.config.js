baa7a7783f83df569d758dc69c58bf652f719168
Zack Walmer <walmerz@gmail.com>
2026-01-18 23:11:47 -0500

    Increment build number to 29 for TestFlight deployment

--- app.config.js
+++ app.config.js
@@ -32,7 +32,7 @@ export default {
         ios: {
             supportsTablet: true,
             bundleIdentifier: "com.neurobloom.app",
-            buildNumber: "28",
+            buildNumber: "29",
             infoPlist: {
                 ITSAppUsesNonExemptEncryption: false,
                 NSHealthShareUsageDescription: "NeuroBloom tracks your walking speed, steadiness, and mobility to help monitor your recovery progress.",
